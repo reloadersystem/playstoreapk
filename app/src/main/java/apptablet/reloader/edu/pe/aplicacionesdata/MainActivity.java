cafe9d4458f373f94999120ecda462048dbed7b2
resembrink <suarezfc8@gmail.com>
2019-03-07 16:32:30 -0500

    Initial commit

---
+++ app/src/main/java/apptablet/reloader/edu/pe/aplicacionesdata/MainActivity.java
@@ -0,0 +1,157 @@
+package apptablet.reloader.edu.pe.aplicacionesdata;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String appName;
+
+    String packageName;
+
+    String box;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boolean isAppInstalled = isPackageInstalled("com.ispringsolutions.mplayer" , this.getPackageManager());
+
+        //String data= String.valueOf(isAppInstalled);
+
+        if(!isAppInstalled ) {
+
+
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                    MainActivity.this);
+
+// Setting Dialog Title
+            alertDialog.setTitle("Confirmar Instalación...");
+
+// Setting Dialog Message
+            alertDialog.setMessage("iSpring Play - Desea instalar la Aplicación ?");
+
+// Setting Icon to Dialog
+            alertDialog.setIcon(R.drawable.unnamed);
+
+// Setting Positive "Yes" Btn
+            alertDialog.setPositiveButton("YES",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            appName = "Gmail";
+                            packageName = "com.ispringsolutions.mplayer";
+
+                            box= "com.ispringsolutions.mplayer";
+
+
+                            openApp( getApplicationContext(), appName, packageName);
+                        }
+                    });
+// Setting Negative "NO" Btn
+            alertDialog.setNegativeButton("NO",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Write your code here to execute after dialog
+                            Toast.makeText(getApplicationContext(),
+                                    "SACO Oliveros - Plataforma Virtual usa esta aplicación Comfirme por favor", Toast.LENGTH_SHORT)
+                                    .show();
+                            dialog.cancel();
+                        }
+                    });
+
+// Showing Alert Dialog
+            alertDialog.show();
+
+
+
+        }
+        else
+        {
+            Toast.makeText(this, "Aplicación esta Instalada", Toast.LENGTH_SHORT).show();
+
+            appName = "Gmail";
+            packageName = "com.ispringsolutions.mplayer";
+
+            box= "com.ispringsolutions.mplayer";
+
+
+            openApp( getApplicationContext(), appName, packageName);
+
+            openApp( getApplicationContext(), appName, packageName);
+        }
+
+
+
+    }
+
+    public void startNewActivity(Context context, String packageName) {
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (intent != null) {
+            // We found the activity now start the activity
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        } else {
+            // Bring user to the market or let them choose an app?
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse("market://details?id=" + packageName));
+            context.startActivity(intent);
+        }
+    }
+
+    public static boolean isPackageInstalled(String packageName, PackageManager packageManager) {
+        try {
+            return packageManager.getApplicationInfo(packageName, 0).enabled;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+
+    public void openApp(Context context, String appName, String packageName) {
+        if (isAppInstalled(context, packageName))
+            if (isAppEnabled(context, packageName))
+                context.startActivity(context.getPackageManager().getLaunchIntentForPackage(packageName));
+            else Toast.makeText(context, appName + " app is not enabled.", Toast.LENGTH_SHORT).show();
+
+        startNewActivity(getApplicationContext(),box);
+
+
+        //else Toast.makeText(context, appName + " app is not installed.", Toast.LENGTH_SHORT).show();
+    }
+
+    private static boolean isAppInstalled(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return false;
+    }
+
+    private static boolean isAppEnabled(Context context, String packageName) {
+        boolean appStatus = false;
+        try {
+            ApplicationInfo ai = context.getPackageManager().getApplicationInfo(packageName, 0);
+            if (ai != null) {
+                appStatus = ai.enabled;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return appStatus;
+    }
+
+}
